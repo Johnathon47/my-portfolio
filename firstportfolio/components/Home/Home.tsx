d13e3f3acfb53e86b9bbdf4508bbc486158eb3e8
Johnathon47 <hei.johnathon.2@gmail.com>
2025-05-30 13:44:39 +0300

    feat: add section Hero, About, Project, Contact on my page home

--- firstportfolio/components/Home/Home.tsx
+++ firstportfolio/components/Home/Home.tsx
@@ -1,7 +1,17 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Hero from "./Hero/Hero";
+import Project from "./Project/Project";
+import Skills from "./Skills/Skills";
+
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className="overflow-hidden">
+            <Hero/>
+            <About/>
+            <Project/>
+            <Skills/>
+            <Contact/>
         </div>
     )
 }
