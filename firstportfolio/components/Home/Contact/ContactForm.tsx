b486b58fb74c5f7a2f2f6f835909b58a40c636f5
Johnathon47 <hei.johnathon.2@gmail.com>
2025-05-30 22:45:05 +0300

    feat: add file for Contact Form section

---
+++ firstportfolio/components/Home/Contact/ContactForm.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function ContactForm() {
+    return (
+        <div className="bg-[#1b3566] rounded-lg p-4 sm:p-10">
+            <h1 className="text-bg text-2xl md:text-3xl lg:text-[2.5rem] font-mono ">
+                Let's Work together!
+            </h1>
+            <p className="text-gray-200 mt-3 lg:text-base text-xs md:text-sm">
+                Got a project in mind? I'm ready to contribute
+                with passion and precision. Let's talk!
+            </p>
+            {/* input fields */}
+        </div>
+    );
+};
