3f27652f05d4f7a85f55fc83069af6d6a276c39a
Johnathon47 <hei.johnathon.2@gmail.com>
2025-05-30 13:29:29 +0300

    feat: add data (content my Info, about me, project, skill, etc)

--- firstportfolio/Data/data.ts
+++ firstportfolio/Data/data.ts
@@ -1,14 +1,69 @@
-import { title } from "process";
 
 export const BaseInfo = {
-    name: "Wesley",
-    position: "Student",
+    fullName: "TOTOZAFY Johnathon Wesley",
+    position: "Second-year Student at HEI",
     description:
-    "I'm a student at HEI (Haute École Informatique)",
+    "I'm a passionate second-year Computer Science student at HEI (Haute École Informatique), with a strong interest in data, gaming, and basketball. I'm driven by creativity, performance, and continuous learning.",
     profilePic: "/image/STD23004.JPG",
 };
 
 export const aboutInfo = {
-    title: "Crafting a soft game and passionate by sport and data.",
-    
+    title: "Crafting innovative experiences at the intersection of gaming, data, and sport.",
+    description:
+    "As a curious and creative student, I love exploring the world of data and game development. Whether it's analyzing information or imagining playful user experiences, I enjoy turning ideas into digital reality. Basketball fuels my teamwork spirit, while coding sharpens my focus and precision.",
+    experience:"1+",
+    academicProject:"5+",
+};
+
+export const projectData = [
+    {
+    id:1,
+    image:"/image/patrimony.png",
+    url:"https://patrimoine-economique-1-nl2m.onrender.com",
 }
+];
+
+export const skillsData = [
+    {
+        id:1,
+        title:"HTML",
+        image: "/image/html.svg",
+        percent: "40%",
+    },
+    {
+        id:2,
+        title:"CSS",
+        image: "/image/css.svg",
+        percent: "40%",
+    },
+    {
+        id:3,
+        title:"JAVASCRIPT",
+        image: "/image/js.svg",
+        percent: "50%",
+    },
+    {
+        id:4,
+        title:"PYTHON",
+        image: "/image/python.svg",
+        percent: "10%",
+    },
+    {
+        id:5,
+        title:"TYPESCRIPT",
+        image: "/image/ts.svg",
+        percent: "10%",
+    },
+    {
+        id:6,
+        title:"JAVA",
+        image: "/image/java.svg",
+        percent: "50%",
+    },
+    {
+        id:7,
+        title:"NEXTJS",
+        image: "/image/nextjs.svg",
+        percent: "20%",
+    },
+];
