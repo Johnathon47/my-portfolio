208d6a9194c588017bb3a2a188ad1b96a6680822
Johnathon47 <hei.johnathon.2@gmail.com>
2025-05-30 22:45:57 +0300

    feat: add file for Contact combine info and form

---
+++ firstportfolio/components/Home/Contact/Contact.tsx
@@ -0,0 +1,19 @@
+import ContactForm from "./ContactForm";
+import ContactInfo from "./ContactInfo";
+
+export default function Contact() {
+    return (
+        <div className="pt-16 pb-16 bg-[#01194A] ">
+            <div className="grid grid-cols-1 xl:grid-cols-2 w-[90%] sm:w-[80%] mx-auto items-center gap-10 mt-10">
+            {/* Contact form */}
+            <div>
+                <ContactForm/>
+            </div>
+            {/* Contact Info */}
+            <div>
+                <ContactInfo/>
+            </div>
+            </div>
+        </div>
+    );
+};
